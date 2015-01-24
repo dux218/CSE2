a9a928b45291d9bc9cc33883f24ccc3a686c82a7
dux218 <553734336@qq.com>
2015-01-24 19:49:14 +0000

    WelcomeClass‘

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,45 @@
+////////////////////////////////////////////////////////////////////////////////
+// Duo Xu
+//  2015.1.24
+//  [CSE2] hw01: Welcome Message
+
+// first compile the program 
+//      javac WelcomeClass.java
+// run the program
+//      java WelcomeClass //
+
+// define a class
+ public class WelcomeClass{
+
+// add a main method
+    public static void main(String[] args) {
+        
+// print the statement
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-D--U--X--2--1--8->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("It's my first year in Lehigh University and I have");
+    System.out.println("CSE2 this semester.");
+    
+//test
+
+
+    
+
+
+
+
+
+
+
+    }
+
+ }
+ 
+         
+
