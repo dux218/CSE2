b74413c5207b97bc453762a164e8ca44ad6db7bd
dux218 <553734336@qq.com>
2015-01-24 05:21:38 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,30 @@
+/////////////////////////////////////////////////////////////////////////////
+//Duo Xu
+//lab01-HelloWorld
+//Hello World Java Program
+
+//    first compile the program
+//           javac HelloWorld.Java
+//    run the program 
+//           java HelloWorld     //
+
+//   define a class
+public class HelloWorld{
+    
+//   add main method
+  public static void main(String[] args) {
+    
+// print the statemnt with box around it
+   System.out.println("########################");
+   System.out.println("########################");
+   System.out.println("#                      #");
+   System.out.println("#  Hello World 2015 !! #");
+   System.out.println("#                      #");   
+   System.out.println("########################");
+   System.out.println("########################");
+   System.out.println("testing");
+   
+   
+   //test
+  }
+}
